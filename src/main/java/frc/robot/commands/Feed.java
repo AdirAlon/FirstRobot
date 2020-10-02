83cbca476c9a648f1f8f31b29f44373c597d9b1a
AdirTheKing <Tshrtkui11@gamil.com>
2020-10-02 11:51:23 +0300

    add feeder

---
+++ src/main/java/frc/robot/commands/Feed.java
@@ -0,0 +1,35 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Feeder;
+import frc.robot.subsystems.Gripper;
+
+public class Feed extends CommandBase {
+     Feeder feeder;
+    double timer;
+
+
+    public Feed(Feeder feeder) {
+        this.feeder = feeder;
+        this.timer = Timer.getFPGATimestamp();
+        addRequirements(feeder);
+    }
+
+    @Override
+    public void execute() {
+        feeder.feed();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return (Timer.getFPGATimestamp()-timer >=500);
+    }
+
+    @Override
+    public void end(boolean interruption) {
+        feeder.stopFeeder();
+    }
+}
+
+
